5f172fc6fe0967866a2113158ec28a33c2714d9f
Clive-C <chung.ck@gmail.com>
2021-06-15 16:32:03 -0400

    first commit

---
+++ src/pomodoro/DurationControl.js
@@ -0,0 +1,120 @@
+import React from "react";
+import {secondsToDuration} from "../utils/duration";
+import {minutesToDuration} from "../utils/duration";
+
+const DurationControl = ({session, focusDuration, setFocusDuration, breakDuration, setBreakDuration}) =>{
+
+    const handleFocusIncrease = (click) =>{
+        const newFocusDuration = focusDuration + 300;
+        if (newFocusDuration >= 3600){
+            setFocusDuration(3600);
+        } 
+        else {
+            setFocusDuration(newFocusDuration);
+        }
+    }
+
+    const handleFocusDecrease = (click) =>{
+        const newFocusDuration = focusDuration - 300;
+        if (newFocusDuration <= 300){
+            setFocusDuration(300);
+        }
+        else {
+            setFocusDuration(newFocusDuration);
+        }
+    }
+
+    const handleBreakIncrease = (click) =>{
+        const newBreakDuration = breakDuration + 60;
+        if (newBreakDuration >= 900){
+            setBreakDuration(900);
+        }
+        else {
+            setBreakDuration(newBreakDuration);
+        }
+    }
+    
+    const handleBreakDecrease = (click) =>{
+        const newBreakDuration = breakDuration - 60;
+        if (newBreakDuration <= 60){
+            setBreakDuration(60);
+        }        
+        else {
+            setBreakDuration(newBreakDuration);
+        }
+    }
+
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    <div className="input-group input-group-lg mb-2">
+                        <span className="input-group-text" data-testid="duration-focus">
+                            Focus Duration: {minutesToDuration(focusDuration/60)}
+                        </span>
+
+                        <div className="input-group-append">
+                            {/* TODO: Implement decreasing focus duration and disable during a focus or break session */}
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-testid="decrease-focus"
+                                onClick={handleFocusDecrease}
+                                disabled={session}
+                            >
+                                <span className="oi oi-minus" />
+                            </button>
+                                {/* TODO: Implement increasing focus duration  and disable during a focus or break session */}
+                            <button
+                                type="button"
+                                className="btn btn-secondary"
+                                data-testid="increase-focus"
+                                onClick={handleFocusIncrease}
+                                disabled={session}   
+                            >
+                                <span className="oi oi-plus" />
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="col">
+                    <div className="float-right">
+                        <div className="input-group input-group-lg mb-2">
+                            <span className="input-group-text" data-testid="duration-break">
+                                {/* TODO: Update this text to display the current break session duration */}
+                                Break Duration: {secondsToDuration(breakDuration)}
+                             </span>
+                            <div className="input-group-append">
+                                {/* TODO: Implement decreasing break duration and disable during a focus or break session*/}
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-testid="decrease-break"
+                                    onClick={handleBreakDecrease}
+                                    disable={session}
+                                >
+                                    <span className="oi oi-minus" />
+                                </button>
+                                {/* TODO: Implement increasing break duration and disable during a focus or break session*/}
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-testid="increase-break"
+                                    onClick={handleBreakIncrease}
+                                    disable={session}
+                                >
+                                    <span className="oi oi-plus" />
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+
+
+}
+
+export default DurationControl;
